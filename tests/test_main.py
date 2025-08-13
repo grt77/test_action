907e6ecd62f36e6e338f2aa81d2e642905c15865
Raviteja Gattupalli <36742299+grt77@users.noreply.github.com>
2025-08-13 19:25:32 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,17 @@ from ..maino import sm
 def test_sm():
     assert sm(2,4)==6
     assert sm(2, 7) == 9
+
+
+def exe_cmd_to_file(cmd, output_file, check_status=True):
+    try:
+        with open(output_file, "wb") as f:
+            p = sp.Popen(cmd, stdout=f, stderr=sp.PIPE, shell=True)
+            _, errors = p.communicate()
+
+        if p.returncode != 0 and check_status:
+            raise Exception(f"cmd: {cmd} failed to execute: {errors.decode('utf-8')}")
+        
+        return output_file  # Path to saved file
+    except Exception as e:
+        raise Exception(f"Error executing cmd: {repr(e)}")
